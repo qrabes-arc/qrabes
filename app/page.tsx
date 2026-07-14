f92e5fa01976680edc97ad7440ac904db6d92f77
qrabes-arc <qrabes@gmail.com>
2026-07-14 14:03:05 +0530

    Implement HomePage with various components
    Added multiple components to the HomePage for better content organization.

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,30 @@
+import Hero from "@/components/home/Hero";
+import TrendingTopics from "@/components/home/TrendingTopics";
+import FeaturedStories from "@/components/home/FeaturedStories";
+import LuxuryFeed from "@/components/home/LuxuryFeed";
+import EditorPicks from "@/components/home/EditorPicks";
+import TrendingBrands from "@/components/home/TrendingBrands";
+import VideoReels from "@/components/home/VideoReels";
+import Newsletter from "@/components/home/Newsletter";
 
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+
+      <TrendingTopics />
+
+      <FeaturedStories />
+
+      <LuxuryFeed />
+
+      <EditorPicks />
+
+      <TrendingBrands />
+
+      <VideoReels />
+
+      <Newsletter />
+    </>
+  );
+}
