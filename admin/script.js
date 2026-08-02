afbf570e1323d2b9229ac13868f9cf4bccd08ae9
qrabes-arc <qrabes@gmail.com>
2026-08-02 10:40:27 +0530

    Change API endpoint from /api/publish to /api/publish.js

--- admin/script.js
+++ admin/script.js
@@ -367,7 +367,7 @@ async function publishPost() {
 
         const response =
             await fetch(
-                "/api/publish",
+                "/api/publish.js",
                 {
 
                     method: "POST",
